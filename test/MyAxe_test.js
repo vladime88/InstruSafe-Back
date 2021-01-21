6ed2f5422d85ae40804709d71e0da4b4fe47dc99
vladime88 <vladime@gmail.com>
2021-01-21 10:00:44 +0100

    app et index deleted

--- test/MyAxe_test.js
+++ test/MyAxe_test.js
@@ -4,9 +4,8 @@ const { contract, accounts } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
 
 const MyAxe = contract.fromArtifact('MyAxe');
-
-/* const isSameMember = (_Member, Member) => {
-  return _Member[0] === Member.name && _Member[1] === Member.tel;
+/* const isSameMember = (_member, member) => {
+  return _member[0] === member.name && _member[1] === member.tel;
 };
 */
 
@@ -33,23 +32,23 @@ describe('MyAxe', function () {
     expect(await this.myaxe.symbol()).to.equal(SYMBOL);
   });
 
-  /* it('add and get Member data', async function () {
+  /*
+  it('add and get Member data', async function () {
     await this.app.createMember(USER1.name, USER1.tel, { from: user1 });
-    await this.app.createMember(USER2.name, USER2.tel, { from: user2 });
+    // await this.app.createMember(USER2.name, USER2.tel, { from: user2 });
 
-    const _Member1 = await this.app.getMember(user1);
-    const _Member2 = await this.app.getMember(user2);
-    expect(isSameMember(_Member1, USER1)).to.be.true;
-    expect(isSameMember(_Member2, USER2)).to.be.true;
+    const _member1 = await this.app.getMember(user1);
+    // const _member2 = await this.app.getMember(user2);
+    expect(isSameMember(_member1, USER1)).to.be.true;
+    // expect(isSameMember(_member2, USER2)).to.be.true;
   });
-});
-*/
+  */
 
   it('Member created', async function () {
-    await this.myaxe.createMember(USER1[0], USER1[1], {
+    await this.MyAxe.createMember(USER1[0], USER1[1], {
       from: user1,
     });
-    const UserNew1 = await this.myaxe.getMember({ from: user1 });
+    const UserNew1 = await this.MyAxe.getMember({ from: user1 });
     expect(UserNew1[0]).to.equal(USER1[0]);
     expect(UserNew1[1]).to.equal(USER1[1]);
   });
